e853eb990c018381a15b4fb4d4e709e1a417bed5
KostinMikhail <79811272328@ya.ru>
2021-10-12 19:13:56 +0300

    Chuck_NotWorkingV3

--- app/src/main/java/com/example/newproject/ChuckActivity.kt
+++ app/src/main/java/com/example/newproject/ChuckActivity.kt
@@ -1,99 +1,73 @@
 package com.example.newproject
 
-import android.app.DownloadManager
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.Html
-import android.util.Log
 import android.view.View
 import android.widget.Button
+import android.widget.ProgressBar
 import android.widget.TextView
-import androidx.appcompat.app.AppCompatActivity
-import androidx.lifecycle.ViewModelProvider
-import com.android.volley.Request
-import com.android.volley.Response
-import com.example.newproject.repository.Repository
-import okhttp3.Call
-import okhttp3.Callback
-import okhttp3.OkHttpClient
+import okhttp3.*
 import org.json.JSONObject
 import java.io.IOException
-import java.util.*
 
+class ChuckActivity : AppCompatActivity() {
 
-class ChuckActivity: AppCompatActivity() {
-
- //   private lateinit var viewModel: MainViewModel
-
-    val URL = "https://api.chucknorris.io/jokes/random"
+    val URL = "https://api.icndb.com/jokes/random"
     var okHttpClient: OkHttpClient = OkHttpClient()
+    lateinit var factTv: TextView
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_chuck)
+        setContentView(R.layout.activity_main)
+        var nextBtn = findViewById(R.id.nextBtn) as Button
+        factTv = findViewById(R.id.factTv) as TextView
+        nextBtn.setOnClickListener{
+            loadRandomFact()
+        }
+    }
 
-        var jokeBtn = findViewById(R.id.jokeBtn) as Button //Этой строчки почему-то в видео тоже не было, кнопка сразу начала выпонлять методы. почему?
-        jokeBtn.setOnClickListener {
-            loadRandomFact ()
+    private fun loadRandomFact(){
+        var progressBar = findViewById(R.id.progressBar) as ProgressBar
+        runOnUiThread {
+            progressBar.visibility = View.VISIBLE
         }
 
-
-
-        fun loadRandomFact () {
-            runOnUiThread {
-                progressBar.visibility = View.VISIBLE
+        val request: Request = Request.Builder().url(URL).build()
+        okHttpClient.newCall(request).enqueue(object: Callback {
+            override fun onFailure(call: Call, e: IOException) {
             }
-            val request: Request = Request.Builder().url(URL).build()
-            okHttpClient.newCall(request).enqueue(object: Callback {
-                override fun onFailure(call: Call?, e: IOException) {
+
+            override fun onResponse(call: Call, response: Response) {
+                val json = response.body?.string()
+// поменять API, вытащить клоюч иконки, и в имидж вью загрузить иконку  подсказка для гула ->"imageView downlad image"
+// перенести в основной проект
+                val txt = (JSONObject(json).getJSONObject("value").get("joke")).toString()
+
+
+                runOnUiThread {
+                    progressBar.visibility = View.GONE
+
+                    factTv.text = txt
                 }
 
-                override fun onResponse(call: Call?, response: okhttp3.Response?) {
-                    val json = response?.body()?.string()
-                    val txt  = (JSONObject(json).getJSONObject("value").get("joke")).toString()
-
-                    runOnUiThread {
-                        progressBar.visibility = View.GONE
-                        textView2.text = Html.fromHtml(txt)
-                    }
-                }
-            })
-        }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-  /*  val repository = Repository()
-    val viewModelFactory = MainViewModelFactory(repository)
-    val textView : TextView = findViewById(R.id.textView) as TextView //Этого не было в видео, но я добавил, т.к. хз, как он создаёт текствьюху и потом вызывает её
-    viewModel = ViewModelProvider(this, viewModelFactory).get(MainViewModel::class.java)
-    viewModel.getPost()
-    viewModel.myResponse.observe(this, androidx.lifecycle.Observer { response ->
-        if(response.isSuccessful){
-            Log.d("Response", response.body()?.userID.toString())
-            Log.d("Response", response.body()?.id.toString())
-            textView.text = response.body()?.title!!
-            Log.d("Response", response.body()?.body!!)
-        }else{
-            Log.d("Response", response.errorBody().toString())
-            textView.text = response.code().toString()
-        }
-
-
-
-
-    })
-*/
+            }
+        })
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
